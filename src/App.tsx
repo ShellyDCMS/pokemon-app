e1398eb28fa6aa68cf9a6c5372f646358cd7fca4
shelly_goldblit <shelly_goldblit@dell.com>
2023-12-23 10:51:58 +0200

    implement click next

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import "./App.css";
 
 const App: React.FC = () => {
+  const [index, setIndex] = React.useState(1);
   return (
     <div className="App">
       <header className="App-header">
         <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`}
           className="App-logo"
           alt="logo"
         />
-        <button>Next</button>
+        <button onClick={() => setIndex(index + 1)}>Next</button>
         <button>Prev</button>
       </header>
     </div>

c7f6b97d39c4aee6965c37c84142b5a52782f343
shelly_goldblit <shelly_goldblit@dell.com>
2023-12-23 16:32:14 +0200

    implement show name

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+
+export interface Pokemon {
+  name: string;
+  url: string;
+}
+
+export interface PokemonList {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Pokemon[];
+}
 
 const App: React.FC = () => {
   const [index, setIndex] = React.useState(1);
+  const [pokemons, setPokemons] = React.useState<PokemonList>();
+
+  const fetchPokemons = async () =>
+    await (await fetch("https://pokeapi.co/api/v2/pokemon/?limit=2000")).json();
+  const initPokemonsList = async () => {
+    setPokemons(await fetchPokemons());
+  };
+
+  useEffect(() => {
+    initPokemonsList();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{pokemons?.results[index - 1].name}</h1>
         <img
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`}
           className="App-logo"
